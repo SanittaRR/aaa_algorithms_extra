11e1d5840ac9fd30888026ea0d5a39c12dbee7f8
saniya <sanitta2401@gmail.com>
2022-12-19 01:08:41 +0600

    New iteration (change after comments)

--- solution_2.py
+++ solution_2.py
@@ -3,14 +3,14 @@ import heapq
 
 def merge_k_sorted(arrs: list) -> list:
     total = []
-    heap0 = []
+    heap = []
 
     for i in range(len(arrs)):
-        heapq.heappush(heap0, (arrs[i][0], i, 0))
+        heapq.heappush(heap, (arrs[i][0], i, 0))
 
-    while heap0:
-        val, rows, columns = heapq.heappop(heap0)
+    while heap:
+        val, rows, columns = heapq.heappop(heap)
         total.append(val)
         if columns + 1 < len(arrs[rows]):
-            heapq.heappush(heap0, (arrs[rows][columns + 1], rows, columns + 1))
+            heapq.heappush(heap, (arrs[rows][columns + 1], rows, columns + 1))
     return total
